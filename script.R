2f1dd8dfe3f746b76bc0647ace0ab709142a67d7
rcantillan <ricantillan@uc.cl>
2022-03-29 23:04:49 -0300

    a

--- script.R
+++ script.R
@@ -1,10 +1,11 @@
-credentials::set_github_pat("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")
+credentials::set_github_pat("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")
 library(blogdown)
 new_site(theme = "hugo-apero/hugo-apero", 
          format = "toml",
          sample = FALSE,
          empty_dirs = TRUE)
 
+usethis::create_github_token()
 blogdown::check_config()
 usethis::browse_github()
 blogdown::serve_site()
